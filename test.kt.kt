2f8d4d6d115ee9dcce157a293edc2a8138df15b2
Ashley <faman1346@gmail.com>
2024-11-27 14:07:48 +0800

    更改成print

--- test.kt.kt
+++ test.kt.kt
@@ -1,14 +1,5 @@
 package org.example
 
 fun main(){
-    val list= listOf(
-        Trumpet(8000),
-        MusicPlayer(),
-    )
-    for (Playable in list){
-        if(Playable is Instrument){
-            Playable.tune()
-        }
-        Playable.play()
-    }
+    print("welcome")
 }
